aadd58ae8cd65602191b1672597e8f5009993b55
AkaneTan <69606737+AkaneTan@users.noreply.github.com>
2025-06-23 19:25:22 +0800

    build: Update gradle version to 7.6.5
    * Updated Gradle version to 7.6.5
    * Updated Kotlin version to 2.1.0
    * Updated Coroutines version to 1.10.2
    * Ditched external BuildConfig plugin
    
    Signed-off-by: AkaneTan <69606737+AkaneTan@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,51 +1,87 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jetbrains.kotlin.util.removeSuffixIfPresent
-
-plugins {
-    val kotlinVersion = "1.8.0"
-    kotlin("jvm") version kotlinVersion
-    kotlin("plugin.serialization") version kotlinVersion
-
-    id("com.github.gmazzo.buildconfig") version "3.1.0"
-    id("net.mamoe.mirai-console") version "2.16.0"
-}
+import java.io.File
 
 group = "uk.akane"
 version = "0.1.0"
 
-repositories {
-    if (System.getenv("CI")?.toBoolean() != true) {
-        maven("https://maven.aliyun.com/repository/public") // 阿里云国内代理仓库
-    }
-    mavenCentral()
-}
+plugins {
+    val kotlinVersion = "2.1.0"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.serialization") version kotlinVersion
 
-buildConfig {
-    className("BuildConstants")
-    packageName("uk.akane.fatal")
-    useKotlinOutput()
-
-    buildConfigField("String", "AUTHOR", "\"AkaneTan, PAKTS\"")
-    buildConfigField("String", "MAJOR_VERSION", "\"${project.version}\"")
-    buildConfigField("String", "HASH_VERSION",  '\"' + "git rev-parse --short=7 HEAD".runCommand(workingDir = rootDir) + '\"')
-    buildConfigField("String", "BUILD_TIME", "\"${System.currentTimeMillis()}\"")
+    id("net.mamoe.mirai-console") version "2.16.0"
 }
 
 mirai {
     noTestCore = true
     setupConsoleTestRuntime {
-        // 移除 mirai-core 依赖
         classpath = classpath.filter {
             !it.nameWithoutExtension.startsWith("mirai-core-jvm")
         }
     }
 }
 
+repositories {
+    if (System.getenv("CI")?.toBoolean() != true) {
+        maven("https://maven.aliyun.com/repository/public")
+    }
+    mavenCentral()
+}
+
 dependencies {
     val overflowVersion = "1.0.6"
-    implementation("org.reflections:reflections:0.10.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     compileOnly("top.mrxiaom.mirai:overflow-core-api:$overflowVersion")
     testConsoleRuntime("top.mrxiaom.mirai:overflow-core:$overflowVersion")
+}
+
+open class BuildConfigExtension {
+    private var _className: String = "BuildConfig"
+    private var _packageName: String = ""
+    var className: String
+        set(value) { _className = value }
+        get() = _className
+    var packageName: String
+        set(value) { _packageName = value }
+        get() = _packageName
+    fun BuildConfigExtension.className(className: String) { this.className = className }
+    fun BuildConfigExtension.packageName(packageName: String) { this.packageName = packageName }
+
+    val fields: MutableList<String> = mutableListOf()
+
+    fun buildConfigField(type: String, name: String, value: String) {
+        fields.add("    const val $name: $type = $value")
+    }
+
+    fun generateBuildConstantsFile(): String {
+        val content = StringBuilder()
+        content.appendLine("package $packageName")
+        content.appendLine()
+        content.appendLine("object $className {")
+        fields.forEach { content.appendLine(it) }
+        content.appendLine("}")
+        return content.toString()
+    }
+
+    fun generateBuildConstants(outputDir: File) {
+        val generatedClassContent = generateBuildConstantsFile()
+        outputDir.mkdirs()
+        val file = File(outputDir, "${className}.kt")
+        file.writeText(generatedClassContent)
+    }
+}
+
+fun Project.buildConfig(configure: BuildConfigExtension.() -> Unit) {
+    val extension = extensions.create<BuildConfigExtension>("buildConfig")
+    extension.configure()
+
+    val outputDir = File("${buildDir}/generated/sources")
+    extension.generateBuildConstants(outputDir)
+}
+
+tasks.withType<KotlinCompile> {
+    source("${buildDir}/generated/sources")
 }
 
 fun String.runCommand(
@@ -54,3 +90,13 @@ fun String.runCommand(
     setWorkingDir(workingDir)
     commandLine(split(' '))
 }.standardOutput.asText.get().removeSuffixIfPresent("\n")
+
+buildConfig {
+    className("BuildConstants")
+    packageName("uk.akane.fatal")
+
+    buildConfigField("String", "AUTHOR", "\"AkaneTan, PAKTS\"")
+    buildConfigField("String", "MAJOR_VERSION", "\"${project.version}\"")
+    buildConfigField("String", "HASH_VERSION",  '\"' + "git rev-parse --short=7 HEAD".runCommand(workingDir = rootDir) + '\"')
+    buildConfigField("String", "BUILD_TIME", "\"${System.currentTimeMillis()}\"")
+}
